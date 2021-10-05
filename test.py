241f367c9c1b07d7f292f71b317497d4b1a2650e
Jonathan Layton <jonathanlayton99@gmail.com>
2021-10-04 20:19:46 -0400

    init commit

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 import mipitube
 
-keepers = 'https://www.youtube.com/playlist?list=PLQ7r1UnuOsj4eKbL6cvbuWp66JCF4ChPP'
-# mipitube.download_audio_list(keepers, 'max')
-
-
-url = 'https://www.youtube.com/playlist?list=PLQ7r1UnuOsj4eKbL6cvbuWp66JCF4ChPP'
+personal_playlist = 'https://www.youtube.com/playlist?list=PLQ7r1UnuOsj4eKbL6cvbuWp66JCF4ChPP'
+# mipitube.download_audio_list(personal_playlist, 'max')
 
 # url = 'https://www.youtube.com/watch?v=CSI6cie_Ci8&list=PL0F37A4B366BB6AA2&index=1'
-
-# url = 'https://www.youtube.com/watch?v=6Y4PEqvk0Jg&list=PLQ7r1UnuOsj5D0WaN9dq_FQpioYWpf4Te&index=2'
-mipitube.download_audio_list(url, 'max')
-# mipitube.download_video(url, 'max')
+# mipitube.download_audio_list(url, 'max')
